f838c9e5c33173701b48de148325e7b113ebfb43
dankomod <danilo@komodora.com>
2021-06-22 17:24:22 -0300

    feat: Add clients filtering

--- src/pages/dashboard/Dashboard.vue
+++ src/pages/dashboard/Dashboard.vue
@@ -3,24 +3,28 @@
     <h1 class="text-2xl text-center">Administração</h1>
     <div class="flex flex-wrap">
       <div class="p-5">
-        <h2 class="pb-2 text-xl">Agendamentos</h2>
-        <p>Últimos 30 dias: {{ filterAppointments("past", 30).length }}</p>
-        <p>Últimos 15 dias: {{ filterAppointments("past", 15).length }}</p>
-        <p>Últimos 7 dias: {{ filterAppointments("past", 7).length }}</p>
-        <p>Ontem: {{ filterAppointments("past", 1).length }}</p>
-        <p>Hoje: {{ filterAppointments().length }}</p>
-        <p>Amanhã: {{ filterAppointments("future", 1).length }}</p>
-        <p>Próximos 7 dias: {{ filterAppointments("future", 7).length }}</p>
-        <p>Próximos 15 dias: {{ filterAppointments("future", 15).length }}</p>
-        <p>Próximos 30 dias: {{ filterAppointments("future", 30).length }}</p>
+        <h2 class="pb-2 text-xl">Agendamentos {{ allAppointments.length }}</h2>
+        <p>Últimos 30 dias: {{ filter("appointments", "past", 30).length }}</p>
+        <p>Últimos 15 dias: {{ filter("appointments", "past", 15).length }}</p>
+        <p>Últimos 7 dias: {{ filter("appointments", "past", 7).length }}</p>
+        <p>Ontem: {{ filter("appointments", "past", 1).length }}</p>
+        <p>Hoje: {{ filter("appointments").length }}</p>
+        <p>Amanhã: {{ filter("appointments", "future", 1).length }}</p>
+        <p>Próximos 7 dias: {{ filter("appointments", "future", 7).length }}</p>
+        <!-- <p>
+          Próximos 15 dias: {{ filter("appointments", "future", 15).length }}
+        </p>
+        <p>
+          Próximos 30 dias: {{ filter("appointments", "future", 30).length }}
+        </p> -->
       </div>
       <div class="p-5 pb-2">
-        <h2 class="text-xl">Clientes</h2>
-        <p>Últimos 30 dias: {{}}</p>
-        <p>Últimos 15 dias: {{}}</p>
-        <p>Últimos 7 dias: {{}}</p>
-        <p>Ontem: {{}}</p>
-        <p>Hoje: {{}}</p>
+        <h2 class="pb-2 text-xl">Clientes {{ allClients.length }}</h2>
+        <p>Últimos 30 dias: {{ filter("clients", "past", 30).length }}</p>
+        <p>Últimos 15 dias: {{ filter("clients", "past", 15).length }}</p>
+        <p>Últimos 7 dias: {{ filter("clients", "past", 7).length }}</p>
+        <p>Ontem: {{ filter("clients", "past", 1).length }}</p>
+        <p>Hoje: {{ filter("clients").length }}</p>
       </div>
     </div>
   </div>
@@ -32,14 +36,16 @@ export default {
   data() {
     return {
       allAppointments: [],
+      allClients: [],
     };
   },
   async created() {
-    await this.getAppointments();
+    await this.loadAppointments();
+    await this.loadClients();
   },
   methods: {
     // Requests appointments for current, past and last month
-    async getAppointments() {
+    async loadAppointments() {
       const now = DateTime.now(); // Current YYYY/MM
       const past = now.minus({ months: 1 }); // Past YYYY/MM
       const future = now.plus({ months: 1 }); // Future YYYY/MM
@@ -59,34 +65,49 @@ export default {
         }
       }
     },
-    filterAppointments(when = "now", value = null) {
+    // Returns filtered appointments based on time and number of days. Takes the 'now', 'past' and 'future' time filters.
+    filter(type, when = "now", value = null) {
+      let arrayToFilter = [];
+      let filterKey = "";
+      if (type === "appointments") {
+        arrayToFilter = this.allAppointments;
+        filterKey = "dateTime";
+      } else if (type === "clients") {
+        arrayToFilter = this.allClients;
+        filterKey = "creationDate";
+      }
       const startOfToday = DateTime.now().startOf("day"); // Today, 00h:00m
-      const endOfToday = DateTime.now()
-        .startOf("day")
+      const endOfToday = startOfToday
         .plus({ days: 1 })
         .minus({ milliseconds: 1 }); // Today, 23h:59m:59s:999ms
       // Past Filter
       if (when === "past") {
         const pastTimeFilter = startOfToday.minus({ days: value }); // Today minus number of days argument
-        return this.allAppointments.filter((val) => {
-          const valDateTime = DateTime.fromISO(val.dateTime); // The DateTime of each appointment
+        return arrayToFilter.filter((val) => {
+          const dateTime = DateTime.fromISO(val[filterKey]); // The DateTime of each appointment
           return (
-            valDateTime >= pastTimeFilter && valDateTime < startOfToday // >= Start of first day of the past, < start of current day
+            dateTime >= pastTimeFilter && dateTime < startOfToday // >= Start of first day of the past, < start of current day
           );
         });
         // Future Filter
       } else if (when === "future") {
         const futureTimeFilter = endOfToday.plus({ days: value }); // End of the future day
-        return this.allAppointments.filter((val) => {
-          const valDateTime = DateTime.fromISO(val.dateTime);
-          return valDateTime > endOfToday && valDateTime <= futureTimeFilter; // > End of current day, <= end of future day
+        return arrayToFilter.filter((val) => {
+          const dateTime = DateTime.fromISO(val[filterKey]);
+          return dateTime > endOfToday && dateTime <= futureTimeFilter; // > End of current day, <= end of future day
         });
         // Current date filter
       } else if (when === "now") {
-        return this.allAppointments.filter((val) => {
-          const valDateTime = DateTime.fromISO(val.dateTime);
-          return valDateTime >= startOfToday && valDateTime <= endOfToday; // >= Start of current day, <= End of current day
+        return arrayToFilter.filter((val) => {
+          const dateTime = DateTime.fromISO(val[filterKey]);
+          return dateTime >= startOfToday && dateTime <= endOfToday; // >= Start of current day, <= End of current day
         });
+      }
+    },
+    async loadClients() {
+      const clients = await this.$store.dispatch("clients/loadClients");
+      for (let client of Object.values(clients)) {
+        this.allClients.push(client);
       }
     },
   },
