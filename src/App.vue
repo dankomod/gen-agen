5a7e1af9cd5c36807db45598de899bf28b4ac860
dankomod <danilo@komodora.com>
2021-07-01 17:18:07 -0300

    feat: Auth-based views

--- src/App.vue
+++ src/App.vue
@@ -75,6 +75,7 @@ export default {
   methods: {
     logout() {
       this.$store.dispatch("logout");
+      this.$router.replace("/auth");
     },
   },
 };
--- src/router.js
+++ src/router.js
@@ -4,14 +4,43 @@ import Agenda from "./pages/agenda/Agenda.vue";
 import Auth from "./pages/auth/UserAuth.vue";
 import Clients from "./pages/clients/Clients.vue";
 import Dashboard from "./pages/dashboard/Dashboard.vue";
+import store from "./store";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/dashboard" },
-    { path: "/agenda", name: "Agenda", component: Agenda },
-    { path: "/auth", name: "Auth", component: Auth },
-    { path: "/clients", name: "Clients", component: Clients },
-    { path: "/dashboard", name: "Dashboard", component: Dashboard },
+    {
+      path: "/agenda",
+      name: "Agenda",
+      component: Agenda,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/auth",
+      name: "Auth",
+      component: Auth,
+      meta: { requiresAuth: false },
+    },
+    {
+      path: "/clients",
+      name: "Clients",
+      component: Clients,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      component: Dashboard,
+      meta: { requiresAuth: true },
+    },
   ],
+});
+router.beforeEach(function (to, from, next) {
+  // If the meta value of the destination contains a true requiresAuth value and the user isn't logged in, redirect to the auth page
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next("/auth");
+  } else {
+    next();
+  }
 });
 export default router;
