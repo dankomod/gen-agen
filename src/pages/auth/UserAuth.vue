57cbba9dc3483306384f636cb7caed755cf00a9b
dankomod <danilo@komodora.com>
2021-07-04 13:07:54 -0300

    refactor: Minimial style update

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <form @submit.prevent="submitForm">
-      <div class="wrapper">
+      <div class="flex flex-col items-center">
         <input
           id="one"
           class="hidden"
@@ -18,11 +18,23 @@
           @change="setMode('signup')"
         />
         <div class="tabs">
-          <label id="one-tab" class="tab" for="one">Entrar</label>
-          <label id="two-tab" class="tab" for="two">Cadastrar</label>
+          <label
+            id="one-tab"
+            class="inline-block p-2 mx-1 text-white bg-black rounded-t-sm cursor-pointer "
+            for="one"
+            >Entrar</label
+          >
+          <label
+            id="two-tab"
+            class="inline-block p-2 mx-1 text-white bg-black rounded-t-sm cursor-pointer "
+            for="two"
+            >Cadastrar</label
+          >
         </div>
-        <div class="panels">
-          <div id="one-panel" class="panel">
+        <div
+          class="w-full p-5 overflow-hidden rounded-sm max-w-screen-xs panels"
+        >
+          <div id="one-panel" class="hidden panel">
             <p>Digite seu email e senha.</p>
           </div>
           <div id="two-panel" class="panel">
@@ -109,29 +121,7 @@ export default {
 </script>
 
 <style>
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.tab {
-  cursor: pointer;
-  padding: 10px 20px;
-  margin: 0px 2px;
-  background: #000;
-  display: inline-block;
-  color: #fff;
-  border-radius: 3px 3px 0px 0px;
-}
-.panels {
-  width: 100%;
-  max-width: 500px;
-  border-radius: 3px;
-  overflow: hidden;
-  padding: 20px;
-}
 .panel {
-  display: none;
   animation: fadein 0.8s;
 }
 @keyframes fadein {
